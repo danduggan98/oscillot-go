8624281617edd499b9031b4a3c18de11a535c820
Dan Duggan <danduggan@protonmail.com>
2023-10-13 01:46:36 -0600

    Naming, equals method

--- util/bit_utils.go
+++ util/bit_utils.go
@@ -1,52 +1,65 @@
 package util
 
 ///// Bit manipulation \\\\\
+// TODO - these only work with 32 bit ints - provide a more generic method for other types
 
 // Extract a particular bit from an integer
-// TODO - this only works with 32 bit ints - provide a more generic method for other types
-func Nth_bit(n, data int) bool {
+func NthBit(n, data int) bool {
 	mask := 1 << (31 - n) // Move a single 1 to the position of our bit
 	bit := data & mask    // Isolate the bit
 	return bit != 0       // Convert to bool
 }
 
 // Extract a section of bits from an integer
-func Bit_slice(start, end, data int) []bool {
+func BitSlice(start, end, data int) []bool {
 	shifted_bits := data >> (31 - end) // Shift the desired bits to the end
 	mask := (1 << start) - 1           // Create a string of 1s the length of our section
 	slice := shifted_bits & mask       // Isolate the desired bits
-
+	// TODO - THIS MASK BECOMES ALL ZEROS WHEN START IS 0
 	slice_len := (end - start) + 1
 	slice_idx := 32 - slice_len
 	bit_array := make([]bool, slice_len)
 
 	for i := 0; i < slice_len; i++ {
-		next_bit := Nth_bit(slice_idx+i, slice)
+		next_bit := NthBit(slice_idx+i, slice)
 		bit_array[i] = next_bit
 	}
 	return bit_array
 }
 
+func ToBits(data int) []bool {
+	return BitSlice(0, 31, data)
+}
+
+func Equals(a, b []bool) bool {
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 ///// Bit formatting \\\\\
 
-func Bit_to_string(b bool) string {
+func BitToString(b bool) string {
 	if b {
 		return "1"
 	}
 	return "0"
 }
 
-func Bit_to_int(b bool) int {
+func BitToInt(b bool) int {
 	if b {
 		return 1
 	}
 	return 0
 }
 
-func Bit_array_to_string(arr []bool) string {
+func BitArrayToString(arr []bool) string {
 	str := ""
 	for _, v := range arr {
-		str += Bit_to_string(v)
+		str += BitToString(v)
 	}
 	return str
 }
--- util/bit_utils_test.go
+++ util/bit_utils_test.go
@@ -9,13 +9,13 @@ func TestNthBit(t *testing.T) {
 
 	data := 0xFEC8FEC8 // 1111 1110 1100 1000 1111 1110 1100 1000
 
-	first_bit := Nth_bit(0, data)
-	second_bit := Nth_bit(7, data)
-	third_bit := Nth_bit(8, data)
-	fourth_bit := Nth_bit(15, data)
+	first_bit := NthBit(0, data)
+	second_bit := NthBit(7, data)
+	third_bit := NthBit(8, data)
+	fourth_bit := NthBit(15, data)
 
 	if !first_bit || second_bit || !third_bit || fourth_bit {
-		given_bits := fmt.Sprintf("%d %d %d %d", Bit_to_int(first_bit), Bit_to_int(second_bit), Bit_to_int(third_bit), Bit_to_int(fourth_bit))
+		given_bits := fmt.Sprintf("%d %d %d %d", BitToInt(first_bit), BitToInt(second_bit), BitToInt(third_bit), BitToInt(fourth_bit))
 		t.Fatalf("Bits should be 1 0 1 0, but got " + given_bits)
 	}
 }
@@ -24,17 +24,39 @@ func TestBitSlice(t *testing.T) {
 
 	data := 0xFEC8FEC8 // 1111 1110 1100 1000 1111 1110 1100 1000
 
-	ones_slice := Bit_slice(4, 6, data)
+	ones_slice := BitSlice(4, 6, data)
 	for _, v := range ones_slice {
 		if !v {
-			t.Fatalf("Bits should be all ones, but found a zero: " + Bit_array_to_string(ones_slice))
+			t.Fatalf("Bits should be all ones, but found a zero: " + BitArrayToString(ones_slice))
 		}
 	}
 
-	zeros_slice := Bit_slice(13, 15, data)
+	zeros_slice := BitSlice(13, 15, data)
 	for _, v := range zeros_slice {
 		if v {
-			t.Fatalf("Bits should be all zeros, but found a one: " + Bit_array_to_string(zeros_slice))
+			t.Fatalf("Bits should be all zeros, but found a one: " + BitArrayToString(zeros_slice))
 		}
+	}
+}
+
+func TestEquals(t *testing.T) {
+
+	a := 0xFEC8FEC8
+	b := 0xFEC8FEC8
+
+	fmt.Println(BitArrayToString(ToBits(a)))
+	fmt.Println(BitArrayToString(ToBits(b)))
+
+	if !Equals(ToBits(a), ToBits(b)) {
+		t.Fatalf("bits should be equal")
+	}
+
+	a >>= 1
+
+	fmt.Println(BitArrayToString(ToBits(a)))
+	fmt.Println(BitArrayToString(ToBits(b)))
+
+	if Equals(ToBits(a), ToBits(b)) {
+		t.Fatalf("bits should not be equal")
 	}
 }
