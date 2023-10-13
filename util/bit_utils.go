69bd8f4900c21c1a6f85d791f321da09564df3a5
Dan Duggan <danduggan@protonmail.com>
2023-10-12 21:49:59 -0600

    Correct bit_slice impl

--- util/bit_utils.go
+++ util/bit_utils.go
@@ -1,7 +1,5 @@
 package util
 
-import "fmt"
-
 ///// Bit manipulation \\\\\
 
 // Extract a particular bit from an integer
@@ -9,7 +7,7 @@ import "fmt"
 func Nth_bit(n, data int) bool {
 	mask := 1 << (31 - n) // Move a single 1 to the position of our bit
 	bit := data & mask    // Isolate the bit
-	return bit > 0        // Convert to bool
+	return bit != 0       // Convert to bool
 }
 
 // Extract a section of bits from an integer
@@ -18,23 +16,14 @@ func Bit_slice(start, end, data int) []bool {
 	mask := (1 << start) - 1           // Create a string of 1s the length of our section
 	slice := shifted_bits & mask       // Isolate the desired bits
 
-	fmt.Printf("data: %b\n", data)
-	fmt.Printf("shifted: %b\n", shifted_bits)
-	fmt.Printf("mask: %b\n", mask)
-	fmt.Printf("slice: %b\n", slice)
+	slice_len := (end - start) + 1
+	slice_idx := 32 - slice_len
+	bit_array := make([]bool, slice_len)
 
-	slice_length := (end - start) + 1
-	fmt.Printf("slice_length: %d\n", slice_length)
-
-	bit_array := make([]bool, slice_length)
-
-	for i := slice_length - 1; i >= 0; i-- {
-		next_bit := Nth_bit(31-i, slice)
-		bit_array[slice_length-i-1] = next_bit
-		//bit_array = append(bit_array, next_bit)
-		fmt.Printf("bit #%d: %v\n", i, next_bit)
+	for i := 0; i < slice_len; i++ {
+		next_bit := Nth_bit(slice_idx+i, slice)
+		bit_array[i] = next_bit
 	}
-
 	return bit_array
 }
 
@@ -56,10 +45,8 @@ func Bit_to_int(b bool) int {
 
 func Bit_array_to_string(arr []bool) string {
 	str := ""
-
 	for _, v := range arr {
 		str += Bit_to_string(v)
 	}
-
 	return str
 }
