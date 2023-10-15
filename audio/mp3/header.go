bd5d951aac6ffa5a1431a79eb64b68c86d808706
Dan Duggan <danduggan@protonmail.com>
2023-10-14 23:06:43 -0600

    parse header

--- audio/mp3/header.go
+++ audio/mp3/header.go
@@ -1,10 +1,12 @@
 package mp3
 
+import "github.com/danduggan98/oscillot-go/util"
+
 // TODO - change case of all these to eliminate underscores
 // TODO - clean up doc comments
 type Header struct {
 	// Fixed value used as a searchable entry point to the stream (12 bits)
-	SyncWord [12]bool
+	SyncWord []bool
 
 	// Specifies the MPEG version (1 bit)
 	//  - 1 = MPEG-1
@@ -16,16 +18,16 @@ type Header struct {
 	//  - 01 = Layer 3
 	//  - 10 = Layer 2
 	//  - 11 = Layer 1
-	Layer [2]bool
+	Layer []bool
 
 	// If set, CRC is used for correction (1 bit)
 	ErrorProtection bool
 
 	// The bit rate used to encode the frame (4 bits)
-	BitRate [4]bool
+	BitRate []bool
 
 	// Sampling frequency (2 bits)
-	Frequency [2]bool
+	Frequency []bool
 
 	// Fills extra frame space for streams with 128 kbit/s and 44100 Hz (1 bit)
 	PaddingBit bool
@@ -38,12 +40,12 @@ type Header struct {
 	//  - 01 = Joint stereo
 	//  - 10 = Dual channel
 	//  - 11 = Single channel
-	Mode [2]bool
+	Mode []bool
 
 	// Specifies which methods to use in joint stereo mode (2 bits)
 	//  - First bit = MS stereo on/off
 	//  - Second bit = Intensity stereo on/off
-	ModeExtension [2]bool
+	ModeExtension []bool
 
 	// Specifies if the file is copyrighted (1 bit)
 	Copyrighted bool
@@ -56,8 +58,23 @@ type Header struct {
 	//  - 01 = 50/15 ms
 	//  - 10 = Reserved
 	//  - 11 = CCITT J.17
-	Emphasis [2]bool
+	Emphasis []bool
 }
 
-// TODO - extract individual bits with bit_utils
-//func parse(bits uint32) Header {}
+func ParseHeader(bits int) Header {
+	return Header{
+		SyncWord:        util.BitSlice(0, 11, bits),
+		Version:         util.NthBit(12, bits),
+		Layer:           util.BitSlice(13, 14, bits),
+		ErrorProtection: util.NthBit(15, bits),
+		BitRate:         util.BitSlice(16, 19, bits),
+		Frequency:       util.BitSlice(20, 21, bits),
+		PaddingBit:      util.NthBit(22, bits),
+		PrivacyBit:      util.NthBit(23, bits),
+		Mode:            util.BitSlice(24, 25, bits),
+		ModeExtension:   util.BitSlice(26, 27, bits),
+		Copyrighted:     util.NthBit(28, bits),
+		Original:        util.NthBit(29, bits),
+		Emphasis:        util.BitSlice(30, 31, bits),
+	}
+}
